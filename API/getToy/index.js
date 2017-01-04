a1ea1f6fe37509917544c5c3c9fbdb081307b4a7
Chase Acton <chaseacton@gmail.com>
2017-01-04 14:06:19 -0800

    Added getToy endpoint

---
+++ API/getToy/index.js
@@ -0,0 +1,39 @@
+var mysql = require('mysql');
+
+exports.handler = function(event, context) {
+    var pool = mysql.createPool({
+        connectionLimit: event.dbconnectionlimit,
+        host: event.dbhost,
+        port: event.dbport,
+        user: event.dbuser,
+        password: event.dbpassword,
+        database: event.dbdatabase
+    });
+
+    // Connect to database
+    pool.getConnection(function(err, connection) {
+        if (err) {
+            connection.release();
+            pool.end()
+            return context.fail("Can't connect to DB");
+        }
+
+        // Get the toy's UUID from the URL path
+        var toyUUID = event.toyID;
+
+        connection.query('SELECT * FROM toys WHERE uuid = ? LIMIT 1', [toyUUID],
+            function(err, results, fields) {
+                if (err) {
+                    connection.release();
+                    pool.end()
+                    return context.fail("Query failed: " + err);
+                }
+
+                connection.release();
+                pool.end()
+
+                var toy = results[0];
+                return context.succeed(toy);
+            });
+    });
+};
