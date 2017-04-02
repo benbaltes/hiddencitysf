8ca0c8dd29fd035fb57d6dda6669e69cd5f3c64e
Ben Baltes <bballtez@gmail.com>
2017-04-02 01:42:06 -0700

    Hooked JS up to front page, working on JS for toys page

--- client/catalog.js
+++ client/catalog.js
@@ -4,9 +4,9 @@ $(function() {
 });
 
 function populateAllToys(){
-  $.ajax({ 
-   type : "GET", 
-   url : "https://oxho60p1v9.execute-api.us-west-2.amazonaws.com/dev/toys", 
+  $.ajax({
+   type : "GET",
+   url : "https://oxho60p1v9.execute-api.us-west-2.amazonaws.com/dev/toys",
    beforeSend: function(xhr){xhr.setRequestHeader('x-api-key', 'i7LM89nUBi6csSwEUrNem1sOEOr3eu6B68dch1GR');},
    success : function(result) {
       // Create a div for every single toy returned
@@ -14,28 +14,37 @@ function populateAllToys(){
    		for(toyIndex in result){
         console.log(result[toyIndex])
    			var imageUrl = result[toyIndex].image_url;
+				var toyID = result[toyIndex].uuid;
+				console.log('toyID: ' + toyID )
    			console.log(imageUrl);
-   			$(".toycatalog").append("<div class='box'><div class='toyTitle'></div></div>");
+				//$(".toycatalog").append("<a href=/toys?uuid=" + toyID + ">");
+
+				appendstring1 = "<a href='toy.html?toyID=" + toyID + "'>";
+				appendstring2 = "<div class='box'><div class='toyTitle'></div></div>";
+				appendstring3 = "</a>";
+				toydiv = appendstring1 + appendstring2 + appendstring3;
+
+				$(".toycatalog").append(toydiv);
    		}
 
       // Put image in every single div
-      var childDivs = $(".toycatalog").children();
-      console.log(childDivs)
+      var toyDivs = $(".box")
+      console.log(toyDivs)
       var i = 0
-      for(toyIndex in result){ 
+      for(toyIndex in result){
         // console.log(childDivs[i])
-        // console.log(result[i].image_url)  
-        childDivs[i].style.backgroundImage = "url('" + result[i].image_url + "')";
-        childDivs[i].style.backgroundSize = "cover";
+        // console.log(result[i].image_url)
+        toyDivs[i].style.backgroundImage = "url('" + result[i].image_url + "')";
+        toyDivs[i].style.backgroundSize = "cover";
 
-        var toyTitleDiv = childDivs[i].getElementsByClassName('toyTitle')[0]
+        var toyTitleDiv = toyDivs[i].getElementsByClassName('toyTitle')[0]
         toyTitleDiv.innerHTML = result[i].name;
         i++;
       }
-   }, 
-   error : function(result) { 
+   },
+   error : function(result) {
      	console.log("FAILURE");
      	console.log(result)
-   } 
+   }
  });
 }
--- client/toy.js
+++ client/toy.js
@@ -1,7 +1,10 @@
 $(function() {
-	console.log('Start')
   var toyID = getParameterByName('toyID');
-	populateToyInfo('toyID');
+
+	populateToyInfo(toyID);
+	populateToyLocations(toyID)
+
+
 });
 
 function getParameterByName(name,url) {
@@ -16,17 +19,98 @@ function getParameterByName(name,url) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
+function initMap(locations) {
+	if(locations != null){
+		console.log('locations');
+		console.log(locations);
+		var uluru = {lat: 37.7960991, lng: -122.4155806};
+		// var uluru2 = {lat: 37.7930991, lng: -122.4195806};
+
+		var map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 13,
+			center: uluru
+		});
+
+
+
+		// for(location in locations){
+		// 	// console.log(': ' + location.lat + ' - ' location.lng);
+		// 	//  marker = new google.maps.Marker({
+		// 	// 		position: new google.maps.LatLng(location.lat,location.lng),
+		// 	// 		map: map
+		// 	// 	});
+		// }
+	}
+}
+
+function populateToyLocations(toyID) {
+ console.log('in func getToyLocations');
+ if(toyID==null){
+	console.log('Toyid is null');
+ 	toyID = 'f83f36c8-d2ad-11e6-b08f-0a52c9c1f137';
+ }
+ else{
+	 $.ajax({
+	   type : "GET",
+	   url : "https://oxho60p1v9.execute-api.us-west-2.amazonaws.com/dev/toys/" + toyID + "/history",
+	   beforeSend: function(xhr){xhr.setRequestHeader('x-api-key', 'i7LM89nUBi6csSwEUrNem1sOEOr3eu6B68dch1GR');},
+	   success : function(result) {
+	 	  console.log("SUCCESS");
+	 	  console.log(result);
+
+			var locs = [{}];
+
+			for(locationMeta in result){
+				console.log('in metadata')
+				var lat = locationMeta.lat;
+				var lon = locationMeta.lon;
+				locs = locs + {lat:lat, lng:lon};
+				var message = locationMeta.message;
+			}
+
+			console.log('locs')
+			console.log(locs);
+			initMap(locs);
+	   },
+	   error : function(result) {
+	     	console.log("FAILURE");
+	     	console.log(result)
+	   }
+	 });
+ }
+}
+
 function populateToyInfo(toyID){
-  $.ajax({ 
-   type : "GET", 
-   url : "https://oxho60p1v9.execute-api.us-west-2.amazonaws.com/dev/toys/" + toyID, 
+  $.ajax({
+   type : "GET",
+   url : "https://oxho60p1v9.execute-api.us-west-2.amazonaws.com/dev/toys/" + toyID,
    beforeSend: function(xhr){xhr.setRequestHeader('x-api-key', 'i7LM89nUBi6csSwEUrNem1sOEOr3eu6B68dch1GR');},
    success : function(result) {
+		 console.log('toyInfo: ');
+		 console.log(result);
+	 	 var toyName = result.name;
+		 var toyIMG = result.image_url;
+		 setToyName(toyName);
+		 setToyImage(toyIMG)
 
-   }, 
-   error : function(result) { 
+   },
+   error : function(result) {
      	console.log("FAILURE");
-     	console.log(result)
-   } 
+     	console.log(result);
+   }
  });
+}
+
+function setToyImage(toyIMG){
+
+}
+
+function setToyName(name){
+	console.log('setting toy name: ' + name);
+	$("#toyname").html(name);
+}
+
+function setToyImage(imgurl){
+	console.log('setting toy img: ' + imgurl);
+	$("#toyIMG").attr('src', imgurl);
 }
